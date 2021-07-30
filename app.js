34fa326e51ec24fbb156c2f17dc3a4a1c1aa7d3f
Marko721 <popmark1002@gmail.com>
2021-07-30 14:06:12 +0200

    update

--- app.js
+++ app.js
@@ -2,24 +2,39 @@ const wordString = document.querySelector(".guess");
 
 const words = [
   "Tuning",
-  "Motivational quotes",
-  "Working hard",
-  "The Lord of the rings",
-  "Healthy habits",
+  // "Motivational quotes",
+  // "Working hard",
+  // "The Lord of the rings",
+  // "Healthy habits",
 ];
 
-const guessedLetters = [];
+let charWordArray = [];
+let underscoreLetter = [];
+let guessedLetters = [];
 
-const checkLetters = () => {};
+// Event Listeners
+document.addEventListener("keypress", (e) => {
+  guessedLetters.push(e.key);
+  console.log(guessedLetters);
 
+  // pozivanje funckije koja proverava
+  checkLetters(guessedLetters, charWordArray);
+});
+
+// Functions
 const generateWord = () => {
+  // uzimanje random reci iz word niza
   const randomNumber = Math.floor(Math.random() * words.length);
-
   const guessWord = words[randomNumber];
-  const [...charWordArray] = guessWord;
 
-  let underscoreLetter = [];
+  // resetovanje nizova kada se dugme ponovo klikne
+  charWordArray = [];
+  underscoreLetter = [];
 
+  // pretvaranje random reci u niz slova
+  [...charWordArray] = guessWord;
+
+  // uzimanje svakog slova iz charWordArray i menjati za _ umesto " "
   for (const char of charWordArray) {
     if (char === " ") {
       underscoreLetter += "  ";
@@ -28,16 +43,17 @@ const generateWord = () => {
     }
   }
 
+  // prikazi izmenjenu rec
   wordString.innerHTML = underscoreLetter;
 };
 
-// console.log(word);
-
-document.addEventListener("keypress", (e) => {
-  // setLetters(e.key);
-  guessedLetters.push(e.key);
-  console.log(guessedLetters);
-
-  // pozivanje funckije koja proverava
-  checkLetters(guessedLetters);
-});
+const checkLetters = (guessedLetters, charWordArray) => {
+  for (letter of guessedLetters) {
+    // for loop nesto ne valja ovde
+    if (charWordArray.includes(letter)) {
+      const index = charWordArray.indexOf(letter);
+      underscoreLetter[index] = letter;
+      wordString.innerHTML = underscoreLetter;
+    }
+  }
+};
